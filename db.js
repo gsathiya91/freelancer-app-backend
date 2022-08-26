94d47b868d09a8e5478861ab57331cf3edace469
Sathiya priya <97939755+gsathiya91@users.noreply.github.com>
2022-08-26 13:21:21 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+dbConnect();
+async function dbConnect() {
+  try {
+    await mongoose.connect(
+        `mongodb+srv://Gsathiya:capstoneproject@cluster0.ktemn.mongodb.net/Freelancer-Webapp?retryWrites=true&w=majority`,
+      { useNewUrlParser: true }
+    );
+    console.log("MongoDb connected successfully");
+  } catch (error) {
+    console.log("MongoDb connection failed");
+  }
+}
+
+module.exports = mongoose;
